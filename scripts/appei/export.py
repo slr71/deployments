7988bce24569c777e4b93f0b82067569b0531537
John Wregglesworth <wregglej@cyverse.org>
2024-05-09 14:39:14 -0700

    Add another export URL for apps and merge the result. The result should include group information

--- scripts/appei/export.py
+++ scripts/appei/export.py
@@ -5,13 +5,44 @@ import pathlib
 import os.path
 import json
 import requests
+from deepmerge import always_merger as merger
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Export an app and tool JSON description")
-    parser.add_argument("--server", action="store", required=True, dest="server", help="The server to export definitions from")
-    parser.add_argument("-i", "--id", action="store", required=True, dest="id", help="The ID of the app to export")
-    parser.add_argument("-s", "--system-id", action="store", required=False, default="de", dest="system_id", help="The system-id to use when exporting an app")
-    parser.add_argument("-o", "--output", action="store", required=False, dest="output", help="The file to store app and tool definitions in")
+    parser = argparse.ArgumentParser(
+        description="Export an app and tool JSON description"
+    )
+    parser.add_argument(
+        "--server",
+        action="store",
+        required=True,
+        dest="server",
+        help="The server to export definitions from",
+    )
+    parser.add_argument(
+        "-i",
+        "--id",
+        action="store",
+        required=True,
+        dest="id",
+        help="The ID of the app to export",
+    )
+    parser.add_argument(
+        "-s",
+        "--system-id",
+        action="store",
+        required=False,
+        default="de",
+        dest="system_id",
+        help="The system-id to use when exporting an app",
+    )
+    parser.add_argument(
+        "-o",
+        "--output",
+        action="store",
+        required=False,
+        dest="output",
+        help="The file to store app and tool definitions in",
+    )
     args = parser.parse_args()
 
     token_filepath = pathlib.Path(
@@ -24,22 +55,44 @@ if __name__ == "__main__":
         print("No token data was found, please run login.py")
         exit(1)
 
-    export_url = "https://{}/terrain/admin/apps/{}/{}/details".format(args.server, args.system_id, args.id)
-    res = requests.get(export_url, headers={"Authorization" : "Bearer {}".format(token_data["access_token"])})
-    if res.status_code > 299 or res.status_code < 200:
-        print("Status code {}: {}".format(res.status_code, res.text))
-        exit(1)
-    app = res.json()
+    details_url = "https://{}/terrain/admin/apps/{}/{}/details".format(
+        args.server, args.system_id, args.id
+    )
+    res = requests.get(
+        details_url,
+        headers={"Authorization": "Bearer {}".format(token_data["access_token"])},
+    )
+    res.raise_for_status()
+    app_details = res.json()
 
-    output_map = {"app" : app, "tools" : []}
+    desc_url = "https://{}/terrain/apps/{}/{}".format(
+        args.server, args.system_id, args.id
+    )
+    desc_res = requests.get(
+        desc_url,
+        headers={"Authorization": "Bearer {}".format(token_data["access_token"])},
+    )
+    desc_res.raise_for_status()
+    desc_data = desc_res.json()
+
+    app = merger.merge(app_details, desc_data)
+
+    output_map = {"app": app, "tools": []}
 
     tool_ids = [t["id"] for t in app["tools"]]
     tool_url_template = "https://{}/terrain/admin/tools/{}"
     for t in tool_ids:
         tool_url = tool_url_template.format(args.server, t)
-        tool_response = requests.get(tool_url, headers={"Authorization": "Bearer {}".format(token_data["access_token"])})
+        tool_response = requests.get(
+            tool_url,
+            headers={"Authorization": "Bearer {}".format(token_data["access_token"])},
+        )
         if res.status_code > 299 or res.status_code < 200:
-            print("Status code {}: {}".format(tool_response.status_code, tool_response.text))
+            print(
+                "Status code {}: {}".format(
+                    tool_response.status_code, tool_response.text
+                )
+            )
             exit(1)
         output_map["tools"].append(tool_response.json())
 
