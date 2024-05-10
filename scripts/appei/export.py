eacf9d7961e8c201bceaedf955eaab671301d199
John Wregglesworth <wregglej@cyverse.org>
2024-05-10 08:58:54 -0700

    Simplify the export by only having a single tool listing. Use merge to make sure no info is lost.

--- scripts/appei/export.py
+++ scripts/appei/export.py
@@ -45,6 +45,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    # Get the token data for auth
     token_filepath = pathlib.Path(
         os.path.expanduser("~/.config/cyverse/discoenv/appei/{}".format(args.server))
     )
@@ -55,6 +56,7 @@ if __name__ == "__main__":
         print("No token data was found, please run login.py")
         exit(1)
 
+    # Get the base app info
     details_url = "https://{}/terrain/admin/apps/{}/{}/details".format(
         args.server, args.system_id, args.id
     )
@@ -65,6 +67,7 @@ if __name__ == "__main__":
     res.raise_for_status()
     app_details = res.json()
 
+    # Get extra app info
     desc_url = "https://{}/terrain/apps/{}/{}".format(
         args.server, args.system_id, args.id
     )
@@ -75,32 +78,27 @@ if __name__ == "__main__":
     desc_res.raise_for_status()
     desc_data = desc_res.json()
 
+    # Merge the extra app info into the base dict.
     app = merger.merge(app_details, desc_data)
 
-    output_map = {"app": app, "tools": []}
-
-    tool_ids = [t["id"] for t in app["tools"]]
+    # Lookup and merge extra tool details
     tool_url_template = "https://{}/terrain/admin/tools/{}"
-    for t in tool_ids:
+    for tool in app["tools"]:
+        t = tool["id"]
         tool_url = tool_url_template.format(args.server, t)
         tool_response = requests.get(
             tool_url,
             headers={"Authorization": "Bearer {}".format(token_data["access_token"])},
         )
-        if res.status_code > 299 or res.status_code < 200:
-            print(
-                "Status code {}: {}".format(
-                    tool_response.status_code, tool_response.text
-                )
-            )
-            exit(1)
-        output_map["tools"].append(tool_response.json())
+        tool_response.raise_for_status()
+        tool = merger.merge(tool, tool_response.json())
 
+    # Write out the fully merged object
     if args.output != None:
         output_path = os.path.expanduser(args.output)
         output_path = os.path.expandvars(output_path)
         output_path = pathlib.Path(output_path)
         with open(output_path, "w") as output_file:
-            json.dump(output_map, output_file, sort_keys=True, indent=2)
+            json.dump(app, output_file, sort_keys=True, indent=2)
     else:
-        print(json.dumps(output_map, sort_keys=True, indent=2))
+        print(json.dumps(app, sort_keys=True, indent=2))
